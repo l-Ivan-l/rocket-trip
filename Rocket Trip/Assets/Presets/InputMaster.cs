a703bdc86f9ce91c62b772a2cec54d26a37e35e5
Iván Almanza <ivan.almanza.g@gmail.com>
2021-03-07 19:26:08 -0600

    Rocket art and movement.

---
+++ Rocket Trip/Assets/Presets/InputMaster.cs
@@ -0,0 +1,176 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Presets/InputMaster.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputMaster : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputMaster()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputMaster"",
+    ""maps"": [
+        {
+            ""name"": ""RocketActions"",
+            ""id"": ""19f3ddbe-b9ec-4bf8-8e59-30ba25f3b110"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""1e666e58-a958-4cd3-8cdb-5fc21ab90f75"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""KeyboardMovement"",
+                    ""id"": ""99a46121-94d4-4aa7-ba2c-456b5e09a0b8"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""b557afba-5060-4d26-9da5-0995758d1436"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""93718610-4102-48b0-b40d-670008de1340"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""51497131-2877-4fee-94c4-efb9fe5a520f"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""3ce03b1b-bfb9-48bf-be5f-0c7adc2e5a8b"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // RocketActions
+        m_RocketActions = asset.FindActionMap("RocketActions", throwIfNotFound: true);
+        m_RocketActions_Move = m_RocketActions.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // RocketActions
+    private readonly InputActionMap m_RocketActions;
+    private IRocketActionsActions m_RocketActionsActionsCallbackInterface;
+    private readonly InputAction m_RocketActions_Move;
+    public struct RocketActionsActions
+    {
+        private @InputMaster m_Wrapper;
+        public RocketActionsActions(@InputMaster wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_RocketActions_Move;
+        public InputActionMap Get() { return m_Wrapper.m_RocketActions; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(RocketActionsActions set) { return set.Get(); }
+        public void SetCallbacks(IRocketActionsActions instance)
+        {
+            if (m_Wrapper.m_RocketActionsActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_RocketActionsActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_RocketActionsActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_RocketActionsActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_RocketActionsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public RocketActionsActions @RocketActions => new RocketActionsActions(this);
+    public interface IRocketActionsActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
---
+++ Rocket Trip/Assets/Scripts/RocketScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour
+{
+    private InputMaster inputMaster;
+    private Transform rocketMesh;
+    private float animSpeed = 100f;
+
+    private Vector2 rocketDir;
+    private Rigidbody rocketBody;
+    public float rocketSpeed = 400f;
+
+    private float steeringAngle = 20f;
+
+    void Awake()
+    {
+        rocketMesh = transform.GetChild(0);
+        rocketBody = GetComponent<Rigidbody>();
+        SetUpInputs();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void SetUpInputs()
+    {
+        inputMaster = new InputMaster();
+    }
+
+    void OnEnable()
+    {
+        inputMaster.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputMaster.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RocketAnimation();
+        rocketDir = inputMaster.RocketActions.Move.ReadValue<Vector2>();
+        RocketRotation(rocketDir.x);
+    }
+
+    void FixedUpdate()
+    {
+        RocketMove(rocketDir);
+    }
+
+    void RocketAnimation()
+    {
+        rocketMesh.Rotate(0f, 0f, animSpeed * Time.deltaTime, Space.Self);
+    }
+
+    void RocketMove(Vector2 _direction)
+    {
+        rocketBody.velocity = _direction * rocketSpeed * Time.fixedDeltaTime;
+    }
+
+    void RocketRotation(float _direction)
+    {
+        Vector3 newRot = transform.eulerAngles;
+        float angle = Mathf.LerpAngle(newRot.z, steeringAngle * -_direction, Time.deltaTime * 10f);
+        newRot.z = angle;
+        transform.eulerAngles = newRot;
+    }
+}
